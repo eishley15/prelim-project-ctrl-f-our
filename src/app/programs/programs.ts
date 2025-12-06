75012e7c0e7f2088bc02196211066b4510e72950
maxqmb <maxxquiambao@gmail.com>
2025-12-06 22:37:05 +0800

    programs

--- src/app/programs/programs.ts
+++ src/app/programs/programs.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Footer } from "../footer/footer";
+import { Header } from '../header/header';
 
 @Component({
   selector: 'app-programs',
-  imports: [],
+  imports: [Footer, Header],
   templateUrl: './programs.html',
   styleUrl: './programs.css',
 })
