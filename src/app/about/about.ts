38de5a7160d27bbd96f197fabc4b3ffd3997f28a
MLapuz30 <lapuzmicah30@gmail.com>
2025-12-08 13:33:52 +0800

    :lipstick: updated style for about page

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [Footer],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
-export class About {
-
+export class About implements OnInit {
+  ngOnInit() {
+    // Scroll to top when navigating to about page
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
